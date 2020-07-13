f3e8bf9da597b3b8d2672f41a06f50395ebdfe3f
Robert Duhig <robertduhig@zoho.com>
2020-07-12 20:22:54 -0400

    implement cycling portraits, credit project teammates

--- app.js
+++ app.js
@@ -10,21 +10,41 @@ app.smoothScroll = () => {
 };
 
 app.animateNav = () => {
-  const $window = $(window);
   const $nav = $("nav");
-
-  $window.scroll(() => {
-    if($window.scrollTop()) {
+  $(window).on("scroll", () => {
+    if($(this).scrollTop()) {
       $nav.attr("class", "compressed");
     } else {
       $nav.removeClass("compressed");
     }
-  });
+  }).trigger("scroll");
+};
+
+app.cycleImages = (imgPaths, $targetImg) => {
+  let index = 1;
+  setInterval(() => {
+    $targetImg.addClass("blur");
+    setTimeout(() => {
+      $targetImg.attr("src", imgPaths[index]).on("load", () => {
+        $targetImg.removeClass("blur");
+      });
+      index = (index + 1) % imgPaths.length;
+    }, 500);
+  }, 5000);
 };
 
 app.init = () => {
+  const portraitPaths = [
+    "./assets/portraits/robert-1.png",
+    "./assets/portraits/robert-2.png",
+    "./assets/portraits/robert-3.png",
+    "./assets/portraits/robert-4.png",
+    "./assets/portraits/robert-5.png"
+  ];
+  $portraitImg = $(".about-image img");
   app.smoothScroll();
   app.animateNav();
+  app.cycleImages(portraitPaths, $portraitImg);
 };
 
 $(() => {
